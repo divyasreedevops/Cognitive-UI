cb14262aa1bf2caa7ee8bf24e8f4ab08d6650481
abdulSohelShaikh <abdulsohel.shaikh@minfytech.com>
2024-09-10 14:43:59 +0530

    Login and multi maps changes done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Demo';
-  headerName = 'Cognitive Navigation';
-  headerIcon = 'assets/images/logo.png';
+  // title = 'Demo';
+  // headerName = 'Cognitive Navigation';
+  // headerIcon = 'assets/images/logo.png';
   activeButton: string = ''; // Track which button is active
+  constructor(public router: Router) {}
 
   setActive(button: string) {
     this.activeButton = button;
+  }
+
+  // Check if the current route is login
+  isLoginRoute() {
+    return this.router.url === '/login';  // Adjust this based on your login route
   }
 }
