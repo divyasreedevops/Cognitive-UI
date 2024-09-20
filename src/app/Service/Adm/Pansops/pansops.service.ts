66f44b1b9c29084a5be6a086c8c884cb140bf2e0
Keerthi-minfy <keerthireddy.karakala@minfytech.com>
2024-09-20 15:53:03 +0530

    panops

--- src/app/Service/Adm/Pansops/pansops.service.ts
+++ src/app/Service/Adm/Pansops/pansops.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class PansopsService {
 
-  constructor() { }
+  private apiUrl = 'https://api.example.com'; // Replace with your API endpoint
+
+  constructor(private http: HttpClient) {}
+
+  
+  getAirports(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/airports`);
+  }
+
+  // Example POST request
+  postData(data: any): Observable<any> {
+    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.post<any>(`${this.apiUrl}/data`, data, { headers });
+  }
+
+  // Example PUT request
+  updateData(id: string, data: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/data/${id}`, data);
+  }
+
+  // Example DELETE request
+  deleteData(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/data/${id}`);
+  }
 }
